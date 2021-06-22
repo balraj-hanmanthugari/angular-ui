b9badee651e2a288398550c569f02da7f065c670
Balraj <balraj.hanmanthugari@gmail.com>
2021-06-22 21:31:39 +0530

    docker changes

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker login -u "$DOCKER_USERNAME" -p "$PASSWORD_PASSWORD"
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
