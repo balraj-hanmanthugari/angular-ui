51f19789ce6cbd5e947845acdae3ae04ad91368b
balraj-hanmanthugari <balraj.hanmanthugari@gmail.com>
2021-07-11 20:39:33 +0530

    header and menu css changes

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    outlet: 'aheader',
+    outlet: 'header',
     component: AppHeaderComponent
   }
 ];
--- src/app/shared/layout/user-header/user-header.component.ts
+++ src/app/shared/layout/user-header/user-header.component.ts
@@ -5,7 +5,7 @@ import { UserService } from '../../../util/user.service';
 @Component({
   selector: 'app-user-header',
   templateUrl: './user-header.component.html',
-  styleUrls: ['./user-header.component.css']
+  styleUrls: ['./user-header.component.scss']
 })
 export class UserHeaderComponent implements OnInit {
   user: any = {};
